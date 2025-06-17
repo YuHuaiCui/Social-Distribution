09fe296e571be32bc8fae661977bcf31f4261ea3
Yang Wang <wang34@ualberta.ca>
2025-06-17 01:39:56 -0600

    modify script final

---
+++ backend/project/settings_heroku.py
@@ -0,0 +1,54 @@
+"""
+Heroku production settings
+"""
+import os
+import dj_database_url
+from .settings import *
+
+# Security
+DEBUG = False
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    os.environ.get('HEROKU_APP_NAME', '') + '.herokuapp.com',
+]
+
+# Database
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
+
+# Static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles'),
+]
+
+# Whitenoise for static files
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Security settings for production
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
